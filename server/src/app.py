3fd8b4e34de4f94f9aac046b61ce94e5a2ca9afc
Joao Vitor <joaomaximo22@hotmail.com>
2022-07-19 14:34:36 -0300

    feat: Adicionar chamada para api no Front

--- server/src/app.py
+++ server/src/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from . import api
 
 
 def create_app():
     """ Criando o App Flask  """
     app = Flask(__name__)
-    
+    CORS(app)
+
     api.init_app(app)
 
     return app
